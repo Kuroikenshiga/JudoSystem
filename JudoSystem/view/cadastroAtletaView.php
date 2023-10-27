8db5a0c0755f4fef3b4cd18160cb74f372678f1f
Salim <salim@example.com>
2023-10-27 17:32:26 -0300

    27-10-23/6

--- JudoSystem/view/cadastroAtletaView.php
+++ JudoSystem/view/cadastroAtletaView.php
@@ -9,22 +9,36 @@
 </head>
 <body>
     <div id="principal">
+        
+            <h1>Cadastro de Atleta</h1>
        
             <form>
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <label for="nome">Nome</label>
+                    <input type="text" class="form-control" id="nome">
                 </div>
+
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    <label for="faixa">Faixa</label>
+                    <input type="text" class="form-control" id="faixa">
                 </div>
-                <div class="form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+                <div class="form-group">
+                    <label for="genero">Gênero</label>
+                    <input type="text" class="form-control" id="genero">
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+
+                <div class="form-group">
+                    <label for="dtnasc">Data Nascimento</label>
+                    <input type="date" class="form-control" id="dtnasc">
+                </div>
+
+                <div class="form-group">
+                    <label for="pontucao">Pontuação</label>
+                    <input type="number" class="form-control" id="pontuacao">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
             </form>
         
     </div>
