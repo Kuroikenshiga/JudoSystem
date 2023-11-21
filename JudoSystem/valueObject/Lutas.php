e6dcee8c2b5c6f7256848353ee8a2681c1057aa1
Salim <salim@example.com>
2023-11-21 17:52:24 -0300

    21-11-23

--- JudoSystem/valueObject/Lutas.php
+++ JudoSystem/valueObject/Lutas.php
@@ -1,17 +1,30 @@
 <?php
     class Lutas{
+        private $id_lutas;
         private $tempo;
         private $hansoku_make;
         private $ganhou;
         private $goldenScore;
+        private $atleta_fk;
+        private $categoria_fk;
 
-        public function __construct($tempo, $hansoku_make, $ganhou, $goldenScore){
+        public function __construct($id_lutas, $tempo, $hansoku_make, $ganhou, $goldenScore, $atleta_fk, $categoria_fk){
+            $this->id_lutas = $id_lutas;
             $this->tempo = $tempo;
             $this->hansoku_make  = $hansoku_make;
             $this->ganhou = $ganhou;
             $this->goldenScore = $goldenScore;
+            $this->atleta_fk = $atleta_fk;
+            $this->categoria_fk = $categoria_fk;
         }
 
+        public function getIdLutas(){
+            return $this->id_lutas;
+        }
+        public function setIdLutas($id_lutas){
+            $this->id_lutas = $id_lutas;
+        }
+        
         public function getTempo(){
             return $this->tempo;
         }
@@ -38,6 +51,20 @@
         }
         public function setGoldenScore($goldenScore){
             $this->goldenScore = $goldenScore;
+        }
+        
+        public function getAtleta_fk(){
+            return $this->atleta_fk;
+        }
+        public function setAtleta_fk($atleta_fk){
+            $this->atleta_fk = $atleta_fk;
+        }
+
+        public function getCategoria_fk(){
+            return $this->categoria_fk;
+        }
+        public function setCategoria_fk($categoria_fk){
+            $this->categoria_fk = $categoria_fk;
         }
     }
 ?>
