5dcb244eb16b2d76d7ad17592ede621877dd9717
YOUR <youramamamamam@gmail.com>
2023-12-04 21:40:16 -0300

    04-12-23/3

--- JudoSystem/view/updateAcademiaView.php
+++ JudoSystem/view/updateAcademiaView.php
@@ -5,9 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../../JudoSystem/view/css/forms.css">
+    <link href="../../JudoSystem/view/assets/css/style.css" rel="stylesheet">
+    <link href="../../JudoSystem/view/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../JudoSystem/view/assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="../../JudoSystem/view/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body onload="initializeForm()">
+    <?php require_once('./JudoSystem/view/header.php') ?>
     <div id="principal">
         <h1>Informações da academia</h1>
             <form id="form">
