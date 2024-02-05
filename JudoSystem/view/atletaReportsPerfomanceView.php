38705824dd9e96eba99931bcf4310434ebdd1eb9
Kuroikenshiga <juniorsilvapalafoz@gmail.com>
2024-02-05 12:22:39 -0300

    05-02-24

--- JudoSystem/view/atletaReportsPerfomanceView.php
+++ JudoSystem/view/atletaReportsPerfomanceView.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Perfomance do atleta</title>
     <link rel="stylesheet" href="../../JudoSystem/view/css/reports.css">
     <link href="../../JudoSystem/view/assets/vendor/aos/aos.css" rel="stylesheet">
     <link href="../../JudoSystem/view/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
